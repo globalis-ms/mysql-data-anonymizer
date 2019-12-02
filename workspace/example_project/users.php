45f5a6f423112bca8965ccfc9f5cbc67f9f989b3
Heng ZENG <zengheng3@foxmail.com>
2019-12-02 10:32:53 +0100

    Version 1.1.3

--- workspace/example_project/users.php
+++ workspace/example_project/users.php
@@ -20,4 +20,10 @@ $table->column('created_at')->replaceByFields(function ($rowData, $generator) {
     return $generator->iso8601($rowData['updated_at']);
 });
 
-$table->column('id')->replaceWithGenerator('uuid', true)->synchronizeColumn(['user_id', 'users_roles']);
+$table->column('id')->replaceWithGenerator('uuid', [], true)->synchronizeColumn(['user_id', 'users_roles']);
+
+$table->doAfterUpdate(function ($rowDataBefore, $rowDataAfter, $generator) {
+    $queries = [];
+    $queries[] = "UPDATE users_roles SET user_id = '" . addslashes($rowDataAfter['id']) . "' WHERE user_id = '" . addslashes($rowDataBefore['id']) . "'";
+    return $queries;
+}, ['users_roles']);
